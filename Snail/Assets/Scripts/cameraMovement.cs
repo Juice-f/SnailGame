84f165857e171b9059b4d529c2f59a092104b66b
AxelMarelius <axel.marelius@gmail.com>
2019-08-17 17:29:57 +0200

    CameraMovement
    CameraMovement and tweaking rollmovement

---
+++ Snail/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public GameObject snail;
+    private Vector2 cameraPosition, playerPos;
+
+    public float camStrayDistance, lerpMultiplier = 15f, distanceToPlayer = 0f;
+   
+    void Start()
+    {
+        snail = GameObject.Find("Snail");
+
+
+    }
+
+    void FixedUpdate()
+    {
+        cameraPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+
+        playerPos = new Vector2(snail.transform.position.x, snail.transform.position.y);
+
+        distanceToPlayer = Mathf.Abs(Mathf.Abs(playerPos.magnitude) - Mathf.Abs(cameraPosition.magnitude));
+
+        Debug.Log("Mathf.Abs(Mathf.Abs(playerPos.magnitude) - Mathf.Abs(cameraPosition.magnitude))");
+
+       
+        
+        transform.position = new Vector3 (Mathf.Lerp(cameraPosition.x, playerPos.x, lerpMultiplier * Time.deltaTime * distanceToPlayer), Mathf.Lerp(cameraPosition.y, playerPos.y, lerpMultiplier * Time.deltaTime), transform.position.z);
+
+            
+        
+
+
+    }
+}
