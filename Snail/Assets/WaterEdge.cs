5c5cac3d0182c97b2fc2a08d377ef98f06bd130c
Eliott <Leskigkrem@gmail.com>
2019-08-18 14:04:26 +0200

    waterend

--- Snail/Assets/WaterEdge.cs
+++ Snail/Assets/WaterEdge.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class WaterEdge : MonoBehaviour
 {
+    [SerializeField] Camera cam;
     [SerializeField] GameObject tinter;
-    bool isActive = true;
+    public bool useME = false;
+    public float range;
     // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
+        if (Vector3.Distance(transform.position, cam.transform.position) <= range)
+        {
+            useME = true;
+            tinter.GetComponent<WaterCamera>().waterLevel = transform.position.y;
+        }
+        if (useME && Vector3.Distance(transform.position, cam.transform.position) >= range) tinter.gameObject.SetActive(false);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            Debug.Log("Player entered");
-            tinter.SetActive(!isActive);
-            isActive = !isActive;
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    Debug.Log("Player entered");
+        //    tinter.SetActive(!isActive);
+        //    isActive = !isActive;
+        //}
     }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(transform.position, range);
+    }
+
 }
