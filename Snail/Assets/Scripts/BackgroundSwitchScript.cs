764a5728dd9102dae326fada55b5e970e0d47d5d
AxelMarelius <axel.marelius@gmail.com>
2019-08-18 13:46:34 +0200

    Main menu Fiddling

---
+++ Snail/Assets/Scripts/BackgroundSwitchScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSwitchScript : MonoBehaviour
+{
+
+    public GameObject PreviousScene, NextScene;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PreviousScene.SetActive(false);
+            NextScene.SetActive(true);
+        }
+
+
+    }
+}
