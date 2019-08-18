be15c040ca8bd7eb585eca8a065f8b9220770253
Eliott <Leskigkrem@gmail.com>
2019-08-18 13:47:03 +0200

    water level

---
+++ Snail/Assets/WaterEdge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEdge : MonoBehaviour
+{
+    [SerializeField] GameObject tinter;
+    bool isActive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered");
+            tinter.SetActive(!isActive);
+            isActive = !isActive;
+        }
+    }
+}
